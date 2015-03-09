075820300886cc4ace6edb9994bc0a4175964a1e
garycrawford <bigdaddy@garycrawford.co.uk>
2015-03-09 20:49:47 +0000

    Updating project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject dockerised-web-app/lein-template "0.1.0"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+  :description "Lein template for generating a dockerised Clojure web-app"
+  :url "https://github.com/garycrawford/lein-dockerised-webapp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "https://github.com/garycrawford/lein-dockerised-webapp"})
